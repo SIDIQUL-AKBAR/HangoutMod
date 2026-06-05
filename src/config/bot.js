5081643516649c6739f5c8fcfcb33a084406bca7
SIDIQUL-AKBAR <siddimaster313@gmail.com>
2026-06-05 08:45:16 +0530

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "HANGOUT KERALA COMMUNITY",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
